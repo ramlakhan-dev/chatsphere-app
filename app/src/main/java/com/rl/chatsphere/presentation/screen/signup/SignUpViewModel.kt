222ebf17958323217c841c2b38c8b8f80bb14f29
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2025-09-02 16:03:20 +0530

    implement signup feature

---
+++ app/src/main/java/com/rl/chatsphere/presentation/screen/signup/SignUpViewModel.kt
@@ -0,0 +1,40 @@
+package com.rl.chatsphere.presentation.screen.signup
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rl.chatsphere.domain.model.AuthResult
+import com.rl.chatsphere.domain.usecase.authusecase.AuthUseCase
+import com.rl.chatsphere.presentation.state.AuthState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val authUseCase: AuthUseCase
+): ViewModel() {
+
+    private val _authState = MutableStateFlow<AuthState>(AuthState.Idle)
+    val authState: StateFlow<AuthState> = _authState.asStateFlow()
+
+    fun signUp(email: String,  password: String) {
+        if (email.isEmpty() || password.isEmpty()) {
+            _authState.value = AuthState.Error("Email and Password can't be empty")
+            return
+        }
+
+        viewModelScope.launch {
+            _authState.value = AuthState.Loading
+
+            val result = authUseCase.signUp(email, password)
+            _authState.value = when(result) {
+                is AuthResult.Authenticated -> AuthState.Authenticated
+                is AuthResult.Unauthenticated -> AuthState.Unauthenticated
+                is AuthResult.Error -> AuthState.Error(result.message)
+            }
+        }
+    }
+}
