f251a4547bfbe08c6f2193fcab753e65010a100b
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2025-09-02 15:18:20 +0530

    add welcome screen

--- app/src/main/java/com/rl/chatsphere/presentation/screen/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/rl/chatsphere/presentation/screen/welcome/WelcomeScreen.kt
@@ -1,7 +1,29 @@
 package com.rl.chatsphere.presentation.screen.welcome
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rl.chatsphere.R
 
 @Composable
 fun WelcomeScreen(
@@ -9,5 +31,82 @@ fun WelcomeScreen(
     onSignUpClick: () -> Unit,
     onSignInClick: () -> Unit
 ) {
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_app_logo),
+                contentDescription = stringResource(id = R.string.app_name),
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.onSurface),
+                modifier = Modifier.size(120.dp)
+            )
+
+            Text(
+                text = stringResource(id = R.string.app_name),
+                color = MaterialTheme.colorScheme.onSurface,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(4.dp)
+            )
+
+            Text(
+                text = stringResource(id = R.string.welcome_message),
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(4.dp)
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+        ) {
+
+            Button(
+                onClick = onSignUpClick,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+                    .padding(vertical = 12.dp),
+                shape = RoundedCornerShape(10.dp)
+                ) {
+                Text(
+                    text = stringResource(id = R.string.sign_up),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Button(
+                onClick = onSignInClick,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+                    .padding(vertical = 12.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    disabledContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    disabledContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.sign_in),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
 
 }
